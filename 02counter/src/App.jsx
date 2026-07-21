ef6ac8430629264d63910756a5375af5d47dc878
Shantanu Ravi <shaanravi133@gmail.com>
2026-07-21 15:20:49 +0530

    new react project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <h2>Counter Value: 5</h2>
+
+      <button>Add Value</button><br/>
+      <button>Remove Value</button>
+    </>
+  )
+}
+
+export default App
