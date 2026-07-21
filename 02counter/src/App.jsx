28bb97def8722c4bda4d272abac3701d9f6927d2
Shantanu Ravi <shaanravi133@gmail.com>
2026-07-21 19:17:35 +0530

    counter project

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -5,15 +5,42 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  
+
+  //this is special type of the function useState(hook), in which counter is a variable and setCounter is a function
+
+  let [counter,setCounter] = useState(0)
+  const addValue = ()=>{
+    // console.log("clicked",counter)
+    counter = counter + 1
+    if(counter <= 20){
+      setCounter(counter)
+    }
+    else if(counter > 20)
+    {
+      counter = 20
+    }
+  }
+  const removeValue = () => {
+    counter = counter - 1
+    if(counter >= 0)
+    {
+      setCounter(counter)
+    }
+    else if(counter < 0)
+    {
+      counter = 0
+    }
+  }
 
   return (
     <>
+    <div>
       <h1>Hello</h1>
-      <h2>Counter Value: 5</h2>
+      <h2>Counter Value: {counter}</h2>
 
-      <button>Add Value</button><br/>
-      <button>Remove Value</button>
+      <button onClick={addValue}>Add Value</button><br/>
+      <button onClick={removeValue}>Remove Value</button>
+    </div>
     </>
   )
 }
