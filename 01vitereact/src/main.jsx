67989ebba182ab6a4d3de865f4d62e5383daf88c
Shantanu Ravi <shaanravi133@gmail.com>
2026-07-21 13:10:48 +0530

    learning how react works internally

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,40 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react'
+
+// const reactElement = {
+//   type:"a",
+//   props:{
+//     href:"https://google.com",
+//     target:"_blank"
+//   },
+//   children:"Click me to visit google"
+// }
+// const anotherElement = (
+//   <a href="https://google.com" target="_blank">Visit Google</a>
+// )
+
+// function MyApp (){
+//   return (
+//     <h4>Injected last</h4>
+//   )
+// }
+
+const reactElement = React.createElement(
+  "a",
+  {href:"https://google.com",target:"_blank"},
+  "This is google link"
+)
+
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <App />
-  // </StrictMode>,
+    // <App />
+  // </StrictMode>
+  // anotherElement
+  // <MyApp />
+  // MyApp()
+  reactElement
+
 )
